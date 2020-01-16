762251ded7bb6abc2264fac29ad7571ca1752301
adrievelyn <adrievelyn@gmail.com>
2020-01-15 21:49:30 -0300

    Updates

---
+++ precache-manifest.3e9b1328445e403a3cb41a176100bf9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4a5bddf65947ca60d0163dc00c0a61",
+    "url": "/hackathon-Sodimac/index.html"
+  },
+  {
+    "revision": "409678e23dd990d98d66",
+    "url": "/hackathon-Sodimac/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b5cd6a0e1b41881435d6",
+    "url": "/hackathon-Sodimac/static/css/main.e14705f3.chunk.css"
+  },
+  {
+    "revision": "409678e23dd990d98d66",
+    "url": "/hackathon-Sodimac/static/js/2.20a8f868.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/hackathon-Sodimac/static/js/2.20a8f868.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5cd6a0e1b41881435d6",
+    "url": "/hackathon-Sodimac/static/js/main.28faedf2.chunk.js"
+  },
+  {
+    "revision": "cee33ab16fec16b095ee",
+    "url": "/hackathon-Sodimac/static/js/runtime-main.afe88b0c.js"
+  }
+]);
